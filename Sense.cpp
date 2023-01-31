5ba6fd79f3c83ff658a5af81a9b5d5b92d9378dd
FaZe337 <123419927+FaZe337@users.noreply.github.com>
2023-01-31 18:41:56 -0300

    Update Sense.cpp

--- Sense.cpp
+++ Sense.cpp
@@ -10,17 +10,22 @@ class Sense
 {
 public:
     void update(Level *level, LocalPlayer *localPlayer, std::vector<Player *> *players, X11Utils *x11Utils)
-   {
-        if (!m_level->isPlayable())
+    {
+        if (!level->isPlayable())
             return;
-        for (int i = 0; i < m_players->size(); i++)
+        for (int i = 0; i < players->size(); i++)
         {
-            Player *player = m_players->at(i);
+            Player *player = players->at(i);
             if (!player->isValid())
                 continue;
-            if (player->getTeamNumber() == m_localPlayer->getTeamNumber())
+            if (player->getTeamNumber() == localPlayer->getTeamNumber())
                 continue;
-
+            if (player->isVisible())
+            {
+                player->setGlowEnable(5);
+                player->setGlowThroughWall(1);
+            }
+            
             //if (player->isVisible()){
 
                 player->setCustomGlow();
@@ -46,13 +51,14 @@ public:
                     player->setCustomGlowRed();
                 }
 
-                
+
             /*}
             else
             {
+                    player->setGlowEnable(7);
+                player->setGlowThroughWall(2);
+            }
                 player->setCustomGlow();
                 player->setCustomGlowRed();
             } */
         }
-    }
-};
