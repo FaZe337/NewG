0d85b3a06a884bb2822492145e2927064be36db2
FaZe337 <123419927+FaZe337@users.noreply.github.com>
2023-01-31 18:02:37 -0300

    Add files via upload

---
+++ Sense.cpp
@@ -0,0 +1,67 @@
+#pragma once
+#include <vector>
+#include "LocalPlayer.cpp"
+#include "Player.cpp"
+#include "Math.cpp"
+#include "Level.cpp"
+#include "X11Utils.cpp"
+
+class Sense
+{
+public:
+    void update(Level *level, LocalPlayer *localPlayer, std::vector<Player *> *players, X11Utils *x11Utils)
+    {
+        if (!level->isPlayable())
+            return;
+        for (int i = 0; i < players->size(); i++)
+        {
+            Player *player = players->at(i);
+            if (!player->isValid())
+                continue;
+            if (player->getTeamNumber() == localPlayer->getTeamNumber())
+                continue;
+            if (player->isVisible())
+            {
+                player->setGlowEnable(5);
+                player->setGlowThroughWall(1);
+            }
+            
+            //if (player->isVisible()){
+
+                player->setCustomGlow();
+                player->setCustomGlowWhite();
+
+                if(player->getShieldValue() <= 10){
+                    player->setCustomGlowGreen();
+                } else
+
+                if(player->getShieldValue() <= 50){
+                    player->setCustomGlowWhite();
+                } else
+
+                if(player->getShieldValue() <= 75){
+                    player->setCustomGlowBlue();
+                } else
+
+                if(player->getShieldValue() <= 100){
+                    player->setCustomGlowPurple();
+                } else
+
+                if(player->getShieldValue() <= 125){
+                    player->setCustomGlowRed();
+                }
+
+
+            /*}
+            else
+            {
+                    player->setGlowEnable(7);
+                player->setGlowThroughWall(2);
+            }
+                player->setCustomGlow();
+                player->setCustomGlowRed();
+            } */
+        }
+    }
+};
+
